a9c2bffa4e3d3bd831528f2c94c3a0a04eba904a
Edycar Reyes <edycarreyes@gmail.com>
2018-05-25 00:38:54 +0200

    se diseña y se configura el navbar con sus routerLink

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MatDialog} from '@angular/material';
+import {MaterialModule} from '../../material.module';
+import {ServicioService} from '../../servicio.service';
+import {auth} from 'firebase/app';
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +13,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService : ServicioService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
